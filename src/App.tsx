ca35fbced3974070671850a879b393148adacc83
Kauacnok <kauaprofessionalcontact@gmail.com>
2022-06-24 20:23:57 -0300

    Event Platform commit full

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Event } from './pages/Event'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
+
+function App() {
+	return (
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+		</ApolloProvider>
+	)
+}
+
+export default App
---
+++ src/components/Lesson.tsx
@@ -0,0 +1,72 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { Link, useParams } from 'react-router-dom'
+
+import classNames from 'classnames'
+
+interface LessonProps {
+	title: string,
+	slug: string,
+	availableAt: Date,
+	type: 'live' | 'class'
+}
+
+export function Lesson(props: LessonProps) {
+	const { slug } = useParams<{ slug: string }>()
+
+	const isLessonAvailable = isPast(props.availableAt)
+	const availableAtDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM 'de' yyyy ' • 'k'h'mm", {
+		locale: ptBR
+	})
+
+	const isActiveLesson = slug === props.slug
+
+	return (
+		<Link to={`/event/lesson/${props.slug}`} className="group">
+			<span className="text-gray-300">
+				{availableAtDateFormatted}
+			</span>
+
+			<div 
+				className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+					'bg-green-500': isActiveLesson
+				})}
+			>
+				<header className="flex items-center justify-between">
+					{isLessonAvailable ? 
+						(
+							<span className={classNames("text-sm text-blue-500 font-medium flex items-center gap-2", {
+								'text-white': isActiveLesson,
+								'text-blue-500': !isActiveLesson
+								})}
+							>
+								<CheckCircle size={20} />
+								Conteúdo Liberado
+							</span>
+						) : (
+							
+							<span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+								<Lock size={20} />
+								Em breve
+							</span>
+						)
+					} 
+					<span className={classNames("text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold", {
+						'border-white': isActiveLesson,
+						'border-green-300': isActiveLesson
+					})}>
+						{props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+					</span>
+				</header>
+
+				<strong className={classNames("text-gray-300 mt-5 block", {
+					'text-white': isActiveLesson,
+					'text-gray-300': !isActiveLesson
+				})}>
+					{props.title}
+				</strong>
+			</div>
+		</Link>
+	)
+}
